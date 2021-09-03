5d65fe05f049cb74ac18db174e491b4d57a52167
Sunil Abraham <s.abraham@gmail.com>
2021-09-03 07:47:47 -0400

    TypeError: realms.ownerOf is not a function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { useWeb3React } from "@web3-react/core";
 import Head from "next/head";
 import Link from "next/link";
+import { useEffect } from "react";
 import Account from "../components/Account";
 import ETHBalance from "../components/ETHBalance";
 import useEagerConnect from "../hooks/useEagerConnect";
+
+import useRealms from "../hooks/useRealms";
+
+const BAG_START = 0;
+const BAG_END = 7999;
+
+type Derivative = {
+  realm: string,
+}
 
 function Home() {
   const { account, library } = useWeb3React();
@@ -11,6 +21,27 @@ function Home() {
   const triedToEagerConnect = useEagerConnect();
 
   const isConnected = typeof account === "string" && !!library;
+
+  const realms = useRealms();
+
+  const bags: Derivative[] = []
+
+  // TypeError: realms.ownerOf is not a function :(
+  console.log(realms)
+
+  useEffect(() => {
+    if (realms) {
+      (async () => {
+        for (let bag = BAG_START; bag <= BAG_END; ++bag) {
+          bags.push({
+            realm: await realms.ownerOf(bag)
+          })
+        }
+      })()
+    }
+  }, [realms])
+
+
 
   return (
     <div>
